9ea984461b445513a9410a045f91cea41e47f752
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-22 06:34:46 +0700

    Delete unused URL path

--- beatmap_collections/urls.py
+++ beatmap_collections/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_page, name='login'),
-    path('register/', views.register_page, name='register'),
     path('new/', views.collection_page, name='new_collection')
 ]
