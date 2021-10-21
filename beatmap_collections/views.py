539aafa4ea6b4bbbcc273e4bc5d9c9cce3332ab3
IU4Nene <sirpot2849@gmail.com>
2021-10-21 14:16:06 +0700

    Add login and register page

--- beatmap_collections/views.py
+++ beatmap_collections/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'beatmap_collections/index.html')
+
+
+def login_page(request):
+    return render(request, 'beatmap_collections/login.html')
+
+
+def register_page(request):
+    return render(request, 'beatmap_collections/register.html')
