1c24a8d369650f5cbd75b5c20e71edb18b9beccc
some random idiot <sittipat2554aa@hotmail.com>
2021-10-22 10:22:58 +0700

    Added docstrings to views.py.

--- beatmap_collections/views.py
+++ beatmap_collections/views.py
@@ -1,20 +1,26 @@
+"""Views of beatmap collection app."""
+
 from django.shortcuts import render
 from .models import *
 
 
 def home(request):
+    """The homepage of the website. It contains a list of beatmaps chosen semi-randomly."""
     collections = Collection.objects.all()
     context = {"collections": collections}
     return render(request, 'beatmap_collections/index.html', context)
 
 
 def login_page(request):
+    """The login page. This is where users can log into their accounts."""
     return render(request, 'beatmap_collections/login.html')
 
 
 def register_page(request):
+    """The user registration page. Users can register a new account here."""
     return render(request, 'beatmap_collections/register.html')
 
 
 def collection_page(request):
+    """Collection creation page. Users can create a new beatmap collection here."""
     return render(request, 'beatmap_collections/collection.html')
