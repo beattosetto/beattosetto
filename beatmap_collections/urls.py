6d5350955fd19cbd7de7c96f319526ca8998fd7b
some random idiot <sittipat2554aa@hotmail.com>
2021-10-22 10:12:55 +0700

    Added a functional thumbnail grabbing to index.html.

--- beatmap_collections/urls.py
+++ beatmap_collections/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('register/', views.register_page, name='register'),
     path('new/', views.collection_page, name='new_collection')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
