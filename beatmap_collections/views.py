4684223209912ba060c82f62f0d9d089396ae377
some random idiot <sittipat2554aa@hotmail.com>
2021-10-22 18:05:57 +0700

    User now have to login before they can access collection creation page.

--- beatmap_collections/views.py
+++ beatmap_collections/views.py
@@ -25,7 +25,9 @@ def register_page(request):
 
 def collection_page(request):
     """Collection creation page. Users can create a new beatmap collection here."""
-    return render(request, 'beatmap_collections/collection.html')
+    if request.user.is_authenticated:
+        return render(request, 'beatmap_collections/collection.html')
+    return HttpResponseRedirect(reverse('home'))
 
 
 def save_collection(request):
