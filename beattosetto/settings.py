554d760abc35de104158df3274af6b4896f50aa5
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-23 02:46:27 +0700

    Change default value of debug config

--- beattosetto/settings.py
+++ beattosetto/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default="key_here")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default="localhost")
 
