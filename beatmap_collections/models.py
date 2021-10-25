b89f4446523b8a8726c1a4dd50c3a82c9b6f2814
some random idiot <sittipat2554aa@hotmail.com>
2021-10-25 21:47:50 +0700

    Modified BeatmapEntry's docstring.

--- beatmap_collections/models.py
+++ beatmap_collections/models.py
@@ -88,7 +88,6 @@ class Beatmap(models.Model):
     """This model represents a beatmap. It contains the beatmap's metadata.
 
     Attributes:
-        user: The User who posted this beatmap.
         beatmap_id: An identification number of this beatmap.
         beatmapset_id: An identification number of the beatmap set that this beatmap is in.
         bpm: The number of beats per minute this beatmap offers.
@@ -123,10 +122,6 @@ class Beatmap(models.Model):
         beatmap_card: The cover art of this beatmap.
         beatmap_list: The thumbnail of this beatmap.
         tags: This beatmap's tag(s). If it have more than 1 tag it will separate with space.
-        description: The description of this beatmap that users write it when adding the beatmap.
-        create_date: The creation date of this beatmap.
-        owner_approved: The collection approval status of this beatmap. Changes to true if the owner of the requested
-                        collection approves the request to add this beatmap.
         collection: The collection that this beatmap is included.
     """
     beatmap_id = models.IntegerField(default=75)
@@ -178,6 +173,11 @@ class BeatmapEntry(models.Model):
         author: The user who suggested this beatmap.
         comment: The comment to why the beatmap should be added.
         add_date: The date when the beatmap was added to the collection.
+        user: The User who posted this beatmap.
+        description: The description of this beatmap that users write it when adding the beatmap.
+        create_date: The creation date of this beatmap.
+        owner_approved: The collection approval status of this beatmap. Changes to true if the owner of the requested
+                        collection approves the request to add this beatmap.
     """
 
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
@@ -190,7 +190,6 @@ class BeatmapEntry(models.Model):
     description = models.TextField(default=None, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
     owner_approved = models.BooleanField(default=False)
-
 
 
 class Profile(models.Model):
