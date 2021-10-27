403c789eee615fb107ff0481422a9f42424fe176
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-27 18:39:18 +0700

    Add users url path to url settings

--- beattosetto/urls.py
+++ beattosetto/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('beatmap_collections.urls'))
+    path('', include('beatmap_collections.urls')),
+    path('', include('users.urls'))
 ]
