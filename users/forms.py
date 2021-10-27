a352bcd57d81e78f13bdfd601f6a618eb7e8fd25
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-27 18:56:37 +0700

    Add form for config Django user's model

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import Profile
 
 
-class ProfileForm(forms.ModelForm):
+class ProfileUpdateForm(forms.ModelForm):
     profile_picture = forms.ImageField(required=False, label='Profile Picture')
 
     class Meta:
         model = Profile
         fields = ['profile_picture']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """Form in user's settings page to update value in default Django User model."""
+    email = forms.EmailField(required=False, label='Email')
+    username = forms.CharField(required=True, label='Username')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
