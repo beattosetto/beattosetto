f19831450155c73df196fd4f47e36f604e74e386
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-28 17:31:41 +0700

    Add max_length text to form

--- beatmap_collections/forms.py
+++ beatmap_collections/forms.py
@@ -5,8 +5,8 @@ from .models import Collection, BeatmapEntry
 class CreateCollectionForm(forms.ModelForm):
     """Form for creating the collection"""
     collection_list = forms.ImageField(required=False, label='Image')
-    name = forms.CharField(required=True, label='Name')
-    description = forms.CharField(required=True, widget=forms.Textarea, label='Description')
+    name = forms.CharField(required=True, label='Name', max_length=50)
+    description = forms.CharField(required=True, widget=forms.Textarea, label='Description', max_length=500)
 
     class Meta:
         model = Collection
@@ -16,7 +16,7 @@ class CreateCollectionForm(forms.ModelForm):
 class AddBeatmapForm(forms.Form):
     """Form for adding beatmap using beatmap ID to the collection"""
     beatmap_id = forms.IntegerField(required=True, label='Beatmap ID')
-    comment = forms.CharField(required=True, label='Comment')
+    comment = forms.CharField(required=True, label='Comment', max_length=500)
 
     class Meta:
         fields = ['beatmap', 'comment']
