ce3c180ef36a7446c12c7d4fee63811eb0d39695
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-02 02:28:24 +0700

    Start template on actions page

---
+++ actions/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('actions/', views.actions, name='actions'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
