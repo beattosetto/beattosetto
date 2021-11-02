a78558820d1f63914772d3e068c34130eee5db74
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-02 17:31:48 +0700

    Add logging to Actions script

--- actions/scripts.py
+++ actions/scripts.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 import requests
@@ -5,8 +6,8 @@ from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 from django.db.models.functions import datetime
 from django.utils import timezone
-
-from actions.models import ActionLog
+from .logging import setup_logger, log_two_handler
+from .models import ActionLog
 from beatmap_collections.models import Beatmap
 from beattosetto.settings import OSU_API_V1_KEY
 from django.utils.timezone import make_aware
@@ -18,7 +19,12 @@ def update_beatmap_action_script(action: ActionLog):
     Parameters:
         action (ActionLog): The ActionLog for tracking the action.
     """
+    # Setup the new logger
+    info_logger = setup_logger('info_log', f'media/{action.log}', 'a', logging.INFO)
+    debug_logger = setup_logger('debug_log', f'media/action_log/log_{action.id}_debug.log', 'a', logging.DEBUG)
+    log_two_handler(info_logger, debug_logger, logging.INFO, "Setup logger complete.")
     beatmap_count = Beatmap.objects.all().count()
+    log_two_handler(info_logger, debug_logger, logging.INFO, f"Prepare to update {beatmap_count} beatmaps.")
     failed = 0
     success = 0
     count = 0
@@ -26,26 +32,32 @@ def update_beatmap_action_script(action: ActionLog):
         count += 1
         action.running_text = f"Updating {beatmap.title}[{beatmap.version}] ({count}/{beatmap_count})"
         action.save()
+        log_two_handler(info_logger, debug_logger, logging.INFO, f"Updating {beatmap.title}[{beatmap.version}] ({count}/{beatmap_count})")
         beatmap_id = beatmap.beatmap_id
         parameter = {'b': beatmap.beatmap_id, 'k': OSU_API_V1_KEY}
+        log_two_handler(info_logger, debug_logger, logging.INFO, f'Requesting beatmap data for {beatmap.title}[{beatmap.version}] ({count}/{beatmap_count})')
         request_data = requests.get("https://osu.ppy.sh/api/get_beatmaps", params=parameter)
         if (request_data.status_code == 200) and (request_data.json() != []):
             try:
                 beatmap_json = request_data.json()[0]
+                log_two_handler(info_logger, debug_logger, logging.INFO, f'Beatmap data received for {beatmap.title}[{beatmap.version}]')
+                debug_logger.debug(f"{beatmap.title}[{beatmap.version}] JSON Data : {beatmap_json}")
 
-                action.running_text = f"Fetching the new beatmap picture of {beatmap.title} [{beatmap.version}] ({count}/{beatmap_count})"
+                action.running_text = f"Fetching the new beatmap picture of {beatmap.title}[{beatmap.version}] ({count}/{beatmap_count})"
                 action.save()
 
                 # Try to delete the old beatmap picture and replace it with a new one
                 try:
                     os.remove(f"media/{beatmap.beatmap_card}")
+                    log_two_handler(info_logger, debug_logger, logging.INFO, f"Deleted old beatmap card picture of {beatmap.title}[{beatmap.version}]")
                 except FileNotFoundError:
-                    pass
+                    log_two_handler(info_logger, debug_logger, logging.WARNING, f"No old beatmap card picture of {beatmap.title}[{beatmap.version}] to delete, pass it.")
 
                 try:
                     os.remove(f"media/{beatmap.beatmap_list}")
+                    log_two_handler(info_logger, debug_logger, logging.INFO, f"Deleted old beatmap list picture of {beatmap.title}[{beatmap.version}]")
                 except FileNotFoundError:
-                    pass
+                    log_two_handler(info_logger, debug_logger, logging.WARNING, f"No old beatmap list picture of {beatmap.title}[{beatmap.version}] to delete, pass it.")
 
                 card_pic = requests.get(
                     f"https://assets.ppy.sh/beatmaps/{beatmap_json['beatmapset_id']}/covers/card.jpg")
@@ -53,6 +65,8 @@ def update_beatmap_action_script(action: ActionLog):
                 card_temp.write(card_pic.content)
                 card_temp.flush()
                 beatmap.beatmap_card.save(f"{beatmap_id}.jpg", File(card_temp), save=True)
+                card_temp.close()
+                log_two_handler(info_logger, debug_logger, logging.INFO, f"Saved new beatmap card picture of {beatmap.title}[{beatmap.version}]")
 
                 list_pic = requests.get(
                     f"https://assets.ppy.sh/beatmaps/{beatmap_json['beatmapset_id']}/covers/list.jpg")
@@ -60,8 +74,11 @@ def update_beatmap_action_script(action: ActionLog):
                 list_temp.write(list_pic.content)
                 list_temp.flush()
                 beatmap.beatmap_list.save(f"{beatmap_id}.jpg", File(list_temp), save=True)
+                list_temp.close()
+                log_two_handler(info_logger, debug_logger, logging.INFO, f"Saved new beatmap list picture of {beatmap.title}[{beatmap.version}]")
 
-                action.running_text = f"Updating the metadata of {beatmap.title} [{beatmap.version}] ({count}/{beatmap_count})"
+                action.running_text = f"Updating the metadata of {beatmap.title}[{beatmap.version}] ({count}/{beatmap_count})"
+                log_two_handler(info_logger, debug_logger, logging.INFO, f"Updating the metadata of {beatmap.title} [{beatmap.version}]")
 
                 beatmap.beatmapset_id = beatmap_json['beatmapset_id']
                 beatmap.title = beatmap_json['title']
@@ -115,11 +132,16 @@ def update_beatmap_action_script(action: ActionLog):
                 beatmap.last_update = make_aware(datetime.datetime.strptime(beatmap_json['last_update'], '%Y-%m-%d %H:%M:%S'))
 
                 beatmap.save()
+                log_two_handler(info_logger, debug_logger, logging.INFO,
+                                f"Saved new metadata of {beatmap.title}[{beatmap.version}]")
                 success += 1
             except Exception as e:
-                print(e)
+                log_two_handler(info_logger, debug_logger, logging.ERROR, f"Error while updating the metadata of {beatmap.title}[{beatmap.version}] :\n{str(e)}")
                 failed += 1
         else:
+            log_two_handler(info_logger, debug_logger, logging.ERROR, f"Failed to fetch beatmap data of {beatmap.title}[{beatmap.version}] from osu! API")
+            debug_logger.error(f"Status Code: {request_data.status_code}")
+            debug_logger.error(f"JSON Data: {request_data.json()}")
             failed += 1
         # To make the API request rate not too rush, we need to add a small delay on request
         time.sleep(5)
@@ -127,3 +149,6 @@ def update_beatmap_action_script(action: ActionLog):
     action.running_text = f"Task running successfully with {success} success and {failed} failed!"
     action.time_finish = timezone.now()
     action.save()
+    log_two_handler(info_logger, debug_logger, logging.INFO,
+                    f"Task running successfully with {success} success and {failed} failed!")
+    log_two_handler(info_logger, debug_logger, logging.INFO, "Action finished, thanks for using beatto-chan services!")
