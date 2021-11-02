d91263a5e05b458dd083a534eb62ff41a201c391
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-02 17:32:07 +0700

    Fix docstring in view

--- actions/views.py
+++ actions/views.py
@@ -1,16 +1,18 @@
 import threading
-
 from django.contrib.auth.decorators import user_passes_test
 from django.core.checks import messages
+from django.core.files.base import ContentFile
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.utils import timezone
 from .models import ActionLog
-from .scripts import *
+from .scripts import update_beatmap_action_script
 
 
 @user_passes_test(lambda u: u.is_superuser or u.is_staff)
 def actions(request):
+    """View for Action menu."""
     context = {
         'action_log': ActionLog.objects.all().order_by('-id')
     }
@@ -20,12 +22,9 @@ def actions(request):
 @user_passes_test(lambda u: u.is_superuser or u.is_staff)
 def update_beatmap_action(request):
     """
-    View for activate the new runner for running update_all_beatmap_action function.
+    View for activate the new runner for running update_beatmap_action_script function.
 
-    This view can only activate by superuser and staff. Mainly activate by Maintainer menu.
-
-    :param request: WSGI request from user.
-    :return: Redirect to maintainer menu with message
+    This view can only activate by superuser and staff.
     """
     action_log = ActionLog()
     action_log.name = "Update all beatmap metadata"
@@ -33,6 +32,7 @@ def update_beatmap_action(request):
     action_log.status = 1
     action_log.start_user = request.user
     action_log.save()
+    action_log.log.save(f"log_{action_log.id}", ContentFile(f'# Log for worker ID : {action_log.id}\n'))
     thread_worker = threading.Thread(target=update_beatmap_action_script, args=[action_log])
     thread_worker.setDaemon(True)
     thread_worker.start()
@@ -42,6 +42,10 @@ def update_beatmap_action(request):
 
 @user_passes_test(lambda u: u.is_superuser or u.is_staff)
 def check_action_log(request, log_id):
+    """
+    View for API that request in Actions page to check the status of the action log.
+    It will return the value that used in live updating the Action status.
+    """
     action = get_object_or_404(ActionLog, id=log_id)
     if action.status == 1 or action.status == 0:
         duration = (timezone.now() - action.time_start).total_seconds()
