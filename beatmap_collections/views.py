c6eda7fb4ed7546a8160108179b0f9ff3045fa8d
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-03 03:02:28 +0700

    Add view for approve and deny beatmap

--- beatmap_collections/views.py
+++ beatmap_collections/views.py
@@ -103,7 +103,7 @@ def edit_collection(request, collection_id):
 
 @login_required
 def beatmap_approval(request, collection_id):
-    """View for approve beatmap that user who is not collection owner want to add to collection."""
+    """View for approve beatmap page that user who is not collection owner want to add to collection."""
     collection = get_object_or_404(Collection, id=collection_id)
     if request.user != collection.author:
         messages.error(request, 'How dare you access this page despite not an owner??? Go away!')
@@ -113,3 +113,33 @@ def beatmap_approval(request, collection_id):
         'beatmap_approve': BeatmapEntry.objects.filter(collection=collection)
     }
     return render(request, 'beatmap_collections/beatmap_approval.html', context)
+
+
+@login_required
+def approve_beatmap(request, collection_id, beatmap_entry_id):
+    """View for approve BeatmapEntry to the collection by changing the owner_approved value to True"""
+    collection = get_object_or_404(Collection, id=collection_id)
+    beatmap_entry = get_object_or_404(BeatmapEntry, id=beatmap_entry_id)
+    if request.user != collection.author:
+        messages.error(request, 'Hehehehe No! Stop there!')
+        return redirect('collection', collection_id=collection_id)
+    if beatmap_entry.owner_approved:
+        messages.error(request, 'This beatmap is already approved!')
+        return redirect('collection', collection_id=collection_id)
+    beatmap_entry.owner_approved = True
+    beatmap_entry.save()
+    messages.success(request, 'Beatmap approved!')
+    return redirect('beatmap_approval', collection_id=collection_id)
+
+
+@login_required
+def deny_beatmap(request, collection_id, beatmap_entry_id):
+    """View for deny BeatmapEntry to the collection by deleting the BeatmapEntry object"""
+    collection = get_object_or_404(Collection, id=collection_id)
+    beatmap_entry = get_object_or_404(BeatmapEntry, id=beatmap_entry_id)
+    if request.user != collection.author:
+        messages.error(request, 'Hehehehe No! Stop there!')
+        return redirect('collection', collection_id=collection_id)
+    beatmap_entry.delete()
+    messages.success(request, 'Beatmap denied!')
+    return redirect('beatmap_approval', collection_id=collection_id)
