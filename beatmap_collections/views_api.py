8cc1ac28c7a4c11fc822ec39ff10baafd00309a5
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-04 14:39:49 +0700

    Add response when no beatmap found

--- beatmap_collections/views_api.py
+++ beatmap_collections/views_api.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 from .functions import get_beatmap_detail
 
 
+@login_required
 def live_beatmap_card(request, beatmap_id):
     """Return an HTTP of the beatmap card"""
     beatmap_detail = get_beatmap_detail(beatmap_id)
@@ -10,4 +13,11 @@ def live_beatmap_card(request, beatmap_id):
         beatmap_card['Cache_Control'] = 'public, max_age=100000'
         beatmap_card['Vary'] = 'Accept-Encoding'
         return beatmap_card
-    return None
+    no_beatmap_response = """
+    <div data-aos="fade-up" data-aos-duration="600" data-aos-once="true">
+    <div class="alert alert-warning">
+        <i class="fas fa-sad-cry" style="color: #664d03;"></i> No beatmap found! Beatto-chan is sad :(
+    </div>
+    </div>
+    """
+    return HttpResponse(no_beatmap_response, content_type='text/html')
