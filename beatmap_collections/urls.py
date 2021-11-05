aefd7fe385167cbd42ed8b76717963ff182185c4
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-05 16:47:00 +0700

    Add delete beatmap path

--- beatmap_collections/urls.py
+++ beatmap_collections/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('collections/<int:collection_id>/approval', views.beatmap_approval, name='beatmap_approval'),
     path('collections/<int:collection_id>/approve/<int:beatmap_entry_id>', views.approve_beatmap, name='approve_beatmap'),
     path('collections/<int:collection_id>/deny/<int:beatmap_entry_id>', views.deny_beatmap, name='deny_beatmap'),
+    path('collections/<int:collection_id>/delete/<int:beatmap_entry_id>', views.delete_beatmap, name='delete_beatmap'),
     path('api/get_demo_card/<int:beatmap_id>', views_api.live_beatmap_card, name='get_demo_card'),
 ]
 
