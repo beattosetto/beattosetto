092ab814c7dfa1509aacff9f04cd616c5257a835
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-07 03:22:27 +0700

    Add template tag to update action status on base template

---
+++ beatmap_collections/templatetags/update_beatmaps_running.py
@@ -0,0 +1,14 @@
+from django import template
+from actions.models import ActionLog
+
+register = template.Library()
+
+
+def update_beatmaps_running():
+    """Return the status on Actions tasks about updating beatmap metadata Action as JavaScript boolean value"""
+    if ActionLog.objects.filter(name="Update all beatmaps metadata", status=1).exists():
+        return "true"
+    return "false"
+
+
+register.simple_tag(update_beatmaps_running)
