cb7d09fa7aaf2efa857c1613fea3e5a090241df8
SainTurDaY27 <poom52344@gmail.com>
2021-11-09 17:25:24 +0700

    Add some docstrings.

--- actions/apps.py
+++ actions/apps.py
@@ -1,6 +1,10 @@
+"""
+This apps.py is for Django apps configuration.
+"""
 from django.apps import AppConfig
 
 
 class ActionsConfig(AppConfig):
+    """Class for actions app configuration."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'actions'
