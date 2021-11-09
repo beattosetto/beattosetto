e11f2b1c231a833be017cfce52b235948f79b41c
SainTurDaY27 <poom52344@gmail.com>
2021-11-09 16:31:29 +0700

    Refactor using pylint.

--- users/apps.py
+++ users/apps.py
@@ -1,7 +1,11 @@
+"""
+This apps.py contain UserConfig class.
+"""
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """UserConfig class contain default auto field & import user's signal."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
