4b546c60b7d1a5ae72f6f72fbf3874654d1f65db
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-10 13:44:31 +0700

    Fix code quality from recommendation

--- beatmap_collections/static/js/live_beatmap_card.js
+++ beatmap_collections/static/js/live_beatmap_card.js
@@ -1,11 +1,6 @@
 function RenderDemoCard(value) {
     // If the value in form is blank, get an API call as the beatmap ID is 0
-    let api_url = '';
-    if (value === '') {
-        api_url = '/api/get_demo_card/0'
-    } else {
-        api_url = '/api/get_demo_card/' + value
-    }
+    let api_url = `/api/get_demo_card/${value || 0}`;
     // Use ajax to get the rendered beatmap card
     $.ajax({url: api_url, success: function(result) {
         document.getElementById('beatmap-card').innerHTML = '<div id="demo-beatmap-card"></div>';
