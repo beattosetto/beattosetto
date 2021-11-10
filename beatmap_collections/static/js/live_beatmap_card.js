694844f424f119cb790af83c9d013ba13139d8ca
Phawit Pornwattanakul <me@helloyeew.dev>
2021-11-10 12:03:34 +0700

    Fix JS to request true url when the field is blank

--- beatmap_collections/static/js/live_beatmap_card.js
+++ beatmap_collections/static/js/live_beatmap_card.js
@@ -1,6 +1,13 @@
 function RenderDemoCard(value) {
+    // If the value in form is blank, get an API call as the beatmap ID is 0
+    let api_url = '';
+    if (value === '') {
+        api_url = '/api/get_demo_card/0'
+    } else {
+        api_url = '/api/get_demo_card/' + value
+    }
     // Use ajax to get the rendered beatmap card
-    $.ajax({url: '/api/get_demo_card/' + value, success: function(result) {
+    $.ajax({url: api_url, success: function(result) {
         document.getElementById('beatmap-card').innerHTML = '<div id="demo-beatmap-card"></div>';
         $('#demo-beatmap-card').replaceWith(result);
         $('.mediPlayer').mediaPlayer();
