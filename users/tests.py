5dffe3f5cd7c036ba27e1a75045472d4ec9ab6a1
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2021-11-17 09:33:39 +0700

    test: Add tests for collections by user pagination

--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@ Tests for views in users app.
 """
 from django.test import TestCase
 from django.urls import reverse
+from beatmap_collections.tests import prepare_collections
 from .models import *
 
 
@@ -41,15 +42,21 @@ class SettingsViewTest(TestCase):
 class ProfileViewTests(TestCase):
     """Tests for the profile view."""
 
-    def setUp(self):
+    # These properties will be initialized in setUpTestData.
+    user = None
+    collections = None
+
+    @classmethod
+    def setUpTestData(cls):
         """Create a dummy user to test with."""
-        self.user = User.objects.create_user(username="Dummy", password="QuakeWorldForever")
+        cls.user = User.objects.create_user(username="Dummy", password="QuakeWorldForever")
+        cls.collections = prepare_collections(amount=20, user=cls.user)
+        cls.profile_url = reverse("profile", args=[cls.user.id])
 
     def test_profile_view_valid_user(self):
         """The profile view should return the requested user's profile page if the user id is valid."""
         # Try to get the user with an id of 1, the dummy user.
-        url = reverse("profile", args=[self.user.id])
-        response = self.client.get(url)
+        response = self.client.get(self.profile_url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/profile.html')
 
@@ -60,3 +67,20 @@ class ProfileViewTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, '404.html')
+
+    def test_paginated(self):
+        """Collections should be paginated."""
+        response = self.client.get(self.profile_url)
+        self.assertQuerysetEqual(response.context['collections'], self.collections[:10])
+        response = self.client.get(self.profile_url, {'page': 2})
+        self.assertQuerysetEqual(response.context['collections'], self.collections[10:])
+
+    def test_paginated_not_integer(self):
+        """If the page number is not an integer, it uses the first page."""
+        response = self.client.get(self.profile_url, {'page': 'ninja'})
+        self.assertQuerysetEqual(response.context['collections'], self.collections[:10])
+
+    def test_paginated_exceed_maximum(self):
+        """If the page number exceeds the maximum, it uses the last page."""
+        response = self.client.get(self.profile_url, {'page': 999})
+        self.assertQuerysetEqual(response.context['collections'], self.collections[10:])
